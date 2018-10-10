96e36343a8f2262e67a558db238634732f67670f
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2018-10-10 13:20:04 +0700

    GDK MWS

--- public/sw.js
+++ public/sw.js
@@ -12,3 +12,47 @@ let files = [
     './images/maps-icon.png',
     './images/coming_soon-512.png',
 ]
+
+//install (caches files)
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(appVersion)
+        .then(cache => {
+            return cache.addAll(files)
+            .catch(err => {
+                console.error('Error adding files to cache', err);
+            })
+        })
+    )
+    console.info('SW Installed');
+    self.skipWaiting();
+})
+
+//activate (manage old caches)
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== appVersion){
+                        console.info('Deleting old cache', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    )
+    return self.clients.claim();
+})
+
+//fetch (control network request)
+self.addEventListener('fetch', event => {
+    console.info('SW fetch', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+        .then(res => {
+            return res || fetch(event.request);
+        })
+    )
+})
