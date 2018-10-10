7b86085b3a8b1e9e70be26f05cd5a52fe1cac582
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2018-10-10 13:00:21 +0700

    GDK MWS

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+let appVersion = 'v1.00';
+
+//file to cache
+let files = [
+    './',
+    './index.html',
+    './images/Custom-Icon-Design-Pretty-Office-9-Font.ico',
+    './images/home-icon.png',
+    './images/WhatsApp Image 2018-08-28 at 19.48.43.jpeg',
+    './images/Martz90-Circle-Calculator.ico',
+    './images/map_parking_icon-01.png',
+    './images/maps-icon.png',
+    './images/coming_soon-512.png',
+]
